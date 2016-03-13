5cb786ad2d83f13e0b17fbc5e7fd1194020e79f3
corybillparrish <corybillparrish@gmail.com>
2016-03-12 19:10:13 -0500

    [errr] - Add error test for appending many stack traces.

--- spec/unit/err.js
+++ spec/unit/err.js
@@ -40,7 +40,7 @@ describe("Err", function () {
         }.bind(this));
     });
 
-    it("it should throw an error with templated message", function (done) {
+    it("it should throw an error with template message", function (done) {
       this.setupErrr = function () {
         this.uniqueId = random.uniqueId();
         this.message = "[%s] Some Error";
@@ -145,7 +145,7 @@ describe("Err", function () {
         }.bind(this));
     });
 
-    it("it should throw an error when appending the new error to the end of the given error.", function (done) {
+    it("it should append the new stack trace to the end of the given error.", function (done) {
       this.setupAppendErrors = function () {
         this.appendError1Id = random.uniqueId();
         this.appendError1Message = `[${this.appendError1Id}] Some Error`;
@@ -175,6 +175,63 @@ describe("Err", function () {
 
           expect(response.stack.split(constants.StackTraceDelimiter).length).eql(2);
           expect(response.stack.split(stringifiedDebugParams).length).eql(2);
+
+          expect(response.stack.indexOf(this.appendError1)).to.be.above(-1);
+          expect(response.stack.indexOf(stack)).to.be.above(-1);
+          done();
+        }.bind(this));
+    });
+
+    it("it should append the new stack trace to the end of the given error when there are many errors already appended.", function (done) {
+      this.setupAppendErrors = function () {
+        this.appendError1Id = random.uniqueId();
+        this.appendError1Message = `[${this.appendError1Id}] Some Error 1`;
+        this.appendError1 = Errr.newError(this.appendError1Message).get();
+
+        this.appendError2Id = random.uniqueId();
+
+        this.appendError2DebugParams = {
+          someParam2: random.uniqueId()
+        };
+
+        this.appendError2Message = `[${this.appendError2Id}] Some Error 2`;
+        this.appendError2 = Errr.newError(this.appendError2Message).debug(this.appendError2DebugParams).appendTo(this.appendError1).get();
+
+        this.appendError3Id = random.uniqueId();
+        this.appendError3Message = `[${this.appendError3Id}] Some Error 3`;
+        this.appendError3 = Errr.newError(this.appendError3Message).appendTo(this.appendError2).get();
+      };
+      this.setupErrr = function () {
+        this.uniqueId = random.uniqueId();
+        this.message = "[%s] Some Error";
+        this.debugParams = {
+          someParam: random.uniqueId()
+        };
+
+        this.errr = Errr.newError(this.message, this.uniqueId)
+          .debug(this.debugParams).appendTo(this.appendError3);
+      };
+
+      this.setupAppendErrors();
+      this.setupErrr();
+      this.setupEntryPoint();
+
+      new Scenario()
+        .withEntryPoint(this.entryPointObject, this.entryPointFunction)
+
+        .test(function (response) {
+          let stack = `Error: [${this.uniqueId}] Some Error\n    at ErrorBuilder._build_ `,
+            stringifiedDebugParams = `${constants.DebugPrefix}${JSON.stringify(this.debugParams, null, 2)}`,
+            stringifiedDebugParamsForError2 = `${constants.DebugPrefix}${JSON.stringify(this.appendError2DebugParams, null, 2)}`;
+
+          expect(response.stack.split(constants.StackTraceDelimiter).length).eql(4);
+
+          expect(response.stack.split(stringifiedDebugParams).length).eql(2);
+          expect(response.stack.split(stringifiedDebugParamsForError2).length).eql(2);
+
+          expect(response.stack.indexOf(this.appendError1)).to.be.above(-1);
+          expect(response.stack.indexOf(this.appendError2)).to.be.above(-1);
+          expect(response.stack.indexOf(this.appendError3)).to.be.above(-1);
           expect(response.stack.indexOf(stack)).to.be.above(-1);
           done();
         }.bind(this));
@@ -202,7 +259,12 @@ describe("Err", function () {
       new Scenario()
         .withEntryPoint(this.entryPointObject, this.entryPointFunction)
 
-        .test(function (err, response) { // eslint-disable-line
+        .test(function (err, response) {
+          if (err) {
+            expect(err).to.be.undefined; //eslint-disable-line
+            expect(err.stack).to.be.undefined; // eslint-disable-line
+          }
+
           let stack = `Error: [${this.uniqueId}] Some Error\n    at ErrorBuilder._build_ `;
 
           expect(response.stack.substring(0, stack.length)).eql(stack);
@@ -210,7 +272,7 @@ describe("Err", function () {
         }.bind(this));
     });
 
-    it("it should throw an error with templated message", function (done) {
+    it("it should throw an error with template message", function (done) {
       this.setupErrr = function () {
         this.uniqueId = random.uniqueId();
         this.message = "[%s] Some Error";
@@ -223,7 +285,12 @@ describe("Err", function () {
       new Scenario()
         .withEntryPoint(this.entryPointObject, this.entryPointFunction)
 
-        .test(function (err, response) { // eslint-disable-line
+        .test(function (err, response) {
+          if (err) {
+            expect(err).to.be.undefined; //eslint-disable-line
+            expect(err.stack).to.be.undefined; // eslint-disable-line
+          }
+
           let stack = `Error: [${this.uniqueId}] Some Error\n    at ErrorBuilder._build_ `;
 
           expect(response.stack.substring(0, stack.length)).eql(stack);
@@ -249,7 +316,12 @@ describe("Err", function () {
       new Scenario()
         .withEntryPoint(this.entryPointObject, this.entryPointFunction)
 
-        .test(function (err, response) { // eslint-disable-line
+        .test(function (err, response) {
+          if (err) {
+            expect(err).to.be.undefined; //eslint-disable-line
+            expect(err.stack).to.be.undefined; // eslint-disable-line
+          }
+
           let stack = `Error: [${this.uniqueId}] Some Error\n    at ErrorBuilder._build_ `,
             stringifiedDebugParams = `${constants.DebugPrefix}${JSON.stringify(this.debugParams, null, 2)}`;
 
@@ -277,7 +349,12 @@ describe("Err", function () {
       new Scenario()
         .withEntryPoint(this.entryPointObject, this.entryPointFunction)
 
-        .test(function (err, response) { // eslint-disable-line
+        .test(function (err, response) {
+          if (err) {
+            expect(err).to.be.undefined; //eslint-disable-line
+            expect(err.stack).to.be.undefined; // eslint-disable-line
+          }
+
           let stack = `Error: [${this.uniqueId}] Some Error\n    at ErrorBuilder._build_ `,
             stringifiedDebugParams = `${constants.DebugPrefix}${JSON.stringify(this.debugParams, null, 2)}`;
 
@@ -305,7 +382,12 @@ describe("Err", function () {
       new Scenario()
         .withEntryPoint(this.entryPointObject, this.entryPointFunction)
 
-        .test(function (err, response) { // eslint-disable-line
+        .test(function (err, response) {
+          if (err) {
+            expect(err).to.be.undefined; //eslint-disable-line
+            expect(err.stack).to.be.undefined; // eslint-disable-line
+          }
+
           let stack = `Error: [${this.uniqueId}] Some Error\n    at ErrorBuilder._build_ `,
             stringifiedDebugParams = `${constants.DebugPrefix}${JSON.stringify(this.debugParams, null, 2)}`;
 
@@ -339,12 +421,77 @@ describe("Err", function () {
       new Scenario()
         .withEntryPoint(this.entryPointObject, this.entryPointFunction)
 
-        .test(function (err, response) { // eslint-disable-line
+        .test(function (err, response) {
+          if (err) {
+            expect(err).to.be.undefined; //eslint-disable-line
+            expect(err.stack).to.be.undefined; // eslint-disable-line
+          }
+
           let stack = `Error: [${this.uniqueId}] Some Error\n    at ErrorBuilder._build_ `,
             stringifiedDebugParams = `${constants.DebugPrefix}${JSON.stringify(this.debugParams, null, 2)}`;
 
           expect(response.stack.split(constants.StackTraceDelimiter).length).eql(2);
           expect(response.stack.split(stringifiedDebugParams).length).eql(2);
+          expect(response.stack.indexOf(stack)).to.be.above(-1);
+          done();
+        }.bind(this));
+    });
+
+    it("it should append the new stack trace to the end of the given error when there are many errors already appended.", function (done) {
+      this.setupAppendErrors = function () {
+        this.appendError1Id = random.uniqueId();
+        this.appendError1Message = `[${this.appendError1Id}] Some Error 1`;
+        this.appendError1 = Errr.newError(this.appendError1Message).get();
+
+        this.appendError2Id = random.uniqueId();
+
+        this.appendError2DebugParams = {
+          someParam2: random.uniqueId()
+        };
+
+        this.appendError2Message = `[${this.appendError2Id}] Some Error 2`;
+        this.appendError2 = Errr.newError(this.appendError2Message).debug(this.appendError2DebugParams).appendTo(this.appendError1).get();
+
+        this.appendError3Id = random.uniqueId();
+        this.appendError3Message = `[${this.appendError3Id}] Some Error 3`;
+        this.appendError3 = Errr.newError(this.appendError3Message).appendTo(this.appendError2).get();
+      };
+      this.setupErrr = function () {
+        this.uniqueId = random.uniqueId();
+        this.message = "[%s] Some Error";
+        this.debugParams = {
+          someParam: random.uniqueId()
+        };
+
+        this.errr = Errr.newError(this.message, this.uniqueId)
+          .debug(this.debugParams).appendTo(this.appendError3);
+      };
+
+      this.setupAppendErrors();
+      this.setupErrr();
+      this.setupEntryPoint();
+
+      new Scenario()
+        .withEntryPoint(this.entryPointObject, this.entryPointFunction)
+
+        .test(function (err, response) {
+          if (err) {
+            expect(err).to.be.undefined; //eslint-disable-line
+            expect(err.stack).to.be.undefined; // eslint-disable-line
+          }
+
+          let stack = `Error: [${this.uniqueId}] Some Error\n    at ErrorBuilder._build_ `,
+            stringifiedDebugParams = `${constants.DebugPrefix}${JSON.stringify(this.debugParams, null, 2)}`,
+            stringifiedDebugParamsForError2 = `${constants.DebugPrefix}${JSON.stringify(this.appendError2DebugParams, null, 2)}`;
+
+          expect(response.stack.split(constants.StackTraceDelimiter).length).eql(4);
+
+          expect(response.stack.split(stringifiedDebugParams).length).eql(2);
+          expect(response.stack.split(stringifiedDebugParamsForError2).length).eql(2);
+
+          expect(response.stack.indexOf(this.appendError1)).to.be.above(-1);
+          expect(response.stack.indexOf(this.appendError2)).to.be.above(-1);
+          expect(response.stack.indexOf(this.appendError3)).to.be.above(-1);
           expect(response.stack.indexOf(stack)).to.be.above(-1);
           done();
         }.bind(this));
